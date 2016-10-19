c393cec395623b8a7040fc6ced3053ff5b37b101
Js41637 ftw <js41637@gmail.com>
2016-10-20 09:24:41 +1100

    Try this

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const request = require('request')
 const cheerio = require('cheerio')
-const { compact, map, last, random } = require('lodash')
-const beautify = require('js-beautify').js_beautify
+const { chain, compact, map, last, random, uniq } = require('lodash') // eslint-disable-line
+const beautify = require('js-beautify')
 const fs = require('fs')
 const { exec } = require('child_process')
 const emojis = require('./emojis')
@@ -12,31 +12,47 @@ const beautifyOptions = {
   end_with_newline: true
 }
 
+const headers = {
+  Cookie: config.cookies.join(';')
+}
+
+const jsRegex = /(templates|analytics|beacon|required_libs)(.js|.php)/
+
+function makeRequests(urls) {
+  return Promise.all(urls.map(url => {
+    return new Promise((resolve, reject) => {
+      request({ url, headers }, (err, resp, body) => {
+        if (!err && body) return resolve(body)
+        else return reject("Error contacting slack")
+      })
+    })
+  }))
+}
+
 function getPageScripts() {
   console.log("Getting page scripts")
   return new Promise((resolve, reject) => {
-    let url = `https://${config.teamName}.slack.com/admin`
-    let headers = {
-      Cookie: config.cookies.join(';')
-    }
-    request({ url, headers }, (err, resp, body) => {
-      if (!err && body) {
-        let $ = cheerio.load(body)
-        let urls = compact(map($('script'), ({ attribs: { src } }) => src && !src.match(/(analytics|beacon|required_libs).js/) ? src : null))
-        console.log("Got", urls.length, "urls")
-        return resolve(urls)
-      } else return reject("Error contacting slack")
-    })
+    let url = `https://${config.teamName}.slack.com`
+    let urls = {}
+
+    makeRequests([`${url}/admin`, `${url}/messages`]).then(([ page1, page2 ]) => {
+      let $ = cheerio.load(page1 + page2) // lmao
+      let js = chain($('script')).map(({ attribs: { src } }) => src && !src.match(jsRegex) ? src : null).compact().uniq().value()
+      let css = chain($('link[type="text/css"]')).map(({ attribs: { href } }) => href && !href.match(/lato/) ? href : null).compact().uniq().value()
+      urls = { js, css }
+      return resolve(urls)
+    }).catch(err => reject(`Error fetching URLS: ${err}`))
   })
 }
 
-function getScripts(urls) {
+function getScripts({ js, css }) {
   console.log("Getting scripts")
-  return Promise.all(urls.map(url => {
+  return Promise.all([...js, ...css].map(url => {
     return new Promise((resolve, reject) => {
       request({ url }, (err, resp, body) => {
         if (!err && body) {
-          return resolve({ name: last(url.split('/')), body })
+          let name = last(url.split('/'))
+          return resolve({ name, body, type: name.endsWith('.js') ? 'js' : 'css' })
         } else return reject()
       })
     })
@@ -45,9 +61,9 @@ function getScripts(urls) {
 
 function writeToDisk(scripts) {
   console.log("Writing to disk")
-  return Promise.all(scripts.map(({ name, body }) => {
+  return Promise.all(scripts.map(({ name, body, type }) => {
     return new Promise(resolve => {
-      fs.writeFile(`./scripts/${name}`, beautify(body, beautifyOptions), resolve)
+      fs.writeFile(`./scripts/${type}/${name}`, beautify[type](body, beautifyOptions), resolve)
     })
   }))
 }
@@ -87,10 +103,25 @@ function startTheMagic() {
   }).catch(err => console.err("Error while doing stuff", err))
 }
 
+function checkDirectories() {
+  fs.stat('./scripts', err => {
+    if (err) {
+      fs.mkdir('./scripts', () => {
+        fs.mkdir('./scripts/js')
+        fs.mkdir('./scripts/css')
+      })
+    } else {
+      fs.stat('./scripts/js', err => err ? fs.mkdir('./scripts/js') : void 0)
+      fs.stat('./scripts/css', err => err ? fs.mkdir('./scripts/css'): void 0)
+    }
+  })
+}
+
 setInterval(() => {
   console.log("Checking for updates")
-  startTheMagic()
+  //startTheMagic()
 }, 1000 * 60 * 60)
 
 console.log("Starting up")
 startTheMagic()
+checkDirectories()
