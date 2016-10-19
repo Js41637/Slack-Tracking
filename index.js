c1da6724aec6f71401f3dae80f3414ee22571813
Js41637 ftw <js41637@gmail.com>
2016-10-19 21:31:26 +1100

    There we go

--- index.js
+++ index.js
@@ -1,74 +1,22 @@
-const request = require('request')
-const cheerio = require('cheerio')
-const { compact, map, last, random } = require('lodash')
-const beautify = require('js-beautify').js_beautify
-const fs = require('fs')
-const { exec } = require('child_process')
-const emojis = require('./emojis')
-const config = require('./config')
+const cluster = require('cluster')
+const moment = require('moment');
 
-const beautifyOptions = {
-  indent_size: 2,
-  end_with_newline: true
-}
+// Override default log function to add timestamps
+["log", "error"].forEach(function(method) {
+  let oldMethod = console[method].bind(console)
+  console[method] = function() { oldMethod.apply(console, [`<${moment().format('YY-MM-DD HH:mm:ssSS')}>`, ...arguments]) }
+})
 
-function getPageScripts() {
-  return new Promise((resolve, reject) => {
-    let url = `https://${config.teamName}.slack.com/admin`
-    let headers = {
-      Cookie: config.cookies.join(';')
+// Reboot the bot on crashes
+if (cluster.isMaster) {
+  cluster.fork()
+
+  cluster.on("exit", function(worker, code) {
+    if (code != 0) {
+      console.error("Worker crashed or was rebooted! Spawning a replacement.")
+      cluster.fork()
     }
-    request({ url, headers }, (err, resp, body) => {
-      if (!err && body) {
-        let $ = cheerio.load(body)
-        let urls = compact(map($('script'), ({ attribs: { src } }) => src && !src.match(/(analytics|beacon|required_libs).js/) ? src : null))
-        return resolve(urls)
-      } else return reject("Error contacting slack")
-    })
   })
+} else {
+  module.exports = require('./main.js')
 }
-
-function getScripts(urls) {
-  return Promise.all(urls.map(url => {
-    return new Promise((resolve, reject) => {
-      request({ url }, (err, resp, body) => {
-        if (!err && body) {
-          return resolve({ name: last(url.split('/')), body })
-        } else return reject()
-      })
-    })
-  }))
-}
-
-function writeToDisk(scripts) {
-  return Promise.all(scripts.map(({ name, body }) => {
-    return new Promise(resolve => {
-      fs.writeFile(`./scripts/${name}`, beautify(body, beautifyOptions), resolve)
-    })
-  }))
-}
-
-function getChanges() {
-  return new Promise((resolve, reject) => {
-    exec('git add ./ && git status --short', (err, stdout) => {
-      if (!err && stdout) {
-        let changes = compact(map(stdout.split('\n'), c => c.trim().replace(/^(.+)?scripts\//, '')))
-        return resolve(changes)
-      } else return reject(err)
-    })
-  })
-}
-
-function pushToGit() {
-  getChanges().then(changes => {
-    console.log(changes)
-    let emoji = emojis[random(0, emojis.length - 1)]
-    let msg = `${emoji} ${changes.join(', ')}`
-    console.log(msg)
-    exec(`git commit -a -m "${msg}"`, (err, stdout) => {
-      console.log(err, stdout)
-    })
-  })
-}
-
-getPageScripts().then(getScripts).then(writeToDisk).then(pushToGit)
