78dd8de3280eb853d4047c7ac3343dcead4a887d
Js41637 ftw <js41637@gmail.com>
2016-10-23 09:46:20 +1100

    Pull changes before updating, add comments, tidy and condense code, and use slack beta channel

--- updater.js
+++ updater.js
@@ -13,27 +13,29 @@ if (!config || !config.teamName || !config.updateInterval || !config.cookies) {
   process.exit()
 }
 
-const ClientReleasesURL = 'http://slack-ssb-updates.global.ssl.fastly.net/releases_x64/RELEASES'
+const clientReleasesURL = 'http://slack-ssb-updates.global.ssl.fastly.net/releases_beta_x64/RELEASES'
 const URL = `https://${config.teamName}.slack.com`
 const headers = { Cookie: config.cookies.join(';') }
 const types = { js: 'Scripts', css: 'Styles' }
 const beautifyOptions = { indent_size: 2, end_with_newline: true }
 const jsRegex = /(templates|analytics|beacon|required_libs)(.js|.php)/
 
-function makeRequests(urls) {
+// Downloads the pages and returns their HTML, requires valid session cookies
+function getPageBodys(urls) {
   return Promise.all(urls.map(url => {
     return new Promise((resolve, reject) => {
       request({ url, headers }, (err, resp, body) => {
         if (!err && body) return resolve(body)
-        else return reject("Error contacting slack")
+        return reject("Error contacting slack")
       })
     })
   }))
 }
 
+// Find all the <script> and <link> sources we want from the pages we downloaded
 function getPageScripts() {
   console.log("Getting page scripts")
-  return makeRequests([`${URL}/admin`, `${URL}/messages`]).then(([ page1, page2 ]) => {
+  return getPageBodys([`${URL}/admin`, `${URL}/messages`]).then(([ page1, page2 ]) => {
     let $ = cheerio.load(page1 + page2) // lmao
     let js = chain($('script')).map(({ attribs: { src: url } }) => url && !url.match(jsRegex) ? { url, type: 'js' } : null).compact().uniq().value()
     let css = chain($('link[type="text/css"]')).map(({ attribs: { href: url } }) => url && !url.match(/lato/) ? { url, type: 'css' } : null).compact().uniq().value()
@@ -42,20 +44,21 @@ function getPageScripts() {
   }).catch(err => Promise.reject(`Error fetching URLS: ${err}`))
 }
 
-function getScripts(urls) {
+// Download all the scripts we found (js & css)
+function getIndividualScripts(urls) {
   console.log("Getting scripts")
   return Promise.all(urls.map(({ url, type }) => {
     return new Promise((resolve, reject) => {
       request({ url }, (err, resp, body) => {
-        if (!err && body) {
-          let name = last(url.split('/'))
-          return resolve({ name, body, type })
-        } else return reject(`Error fetching script ${name}`)
+        if (err || !body) return console.log(`Error fetching script ${url}, ${err}`)
+        let name = last(url.split('/'))
+        return resolve({ name, body, type })
       })
     })
   }))
 }
 
+// Write all scripts to disk, overwriting existing
 function writeToDisk(scripts) {
   console.log("Writing to disk")
   return Promise.all(scripts.map(({ name, body, type }) => {
@@ -63,61 +66,80 @@ function writeToDisk(scripts) {
   }))
 }
 
+// Get list of currently changed files and seperate script commits from client commits
 function getChanges(client) {
   console.log("Getting changes")
   return new Promise((resolve, reject) => {
     let what = client ? 'ClientExtracted/' : './ && git reset ClientExtracted/'
     exec(`git add ${what} && git diff --cached --name-status`, (err, stdout) => {
-      if (!err) {
-        let changes = compact(map(stdout.split('\n'), c => c.trim().replace('\t', ' ')))
-        console.log(`Got ${changes.length} changed files`)
-        return resolve(changes)
-      } else return reject(err)
+      if (err) return reject(err)
+      let changes = compact(map(stdout.split('\n'), c => c.trim().replace('\t', ' ')))
+      console.log(`Got ${changes.length} changed files`)
+      return resolve(changes)
     })
   })
 }
 
+// Create a commit and push to Github
 function pushToGit(client) {
   return new Promise((resolve, reject) => {
     console.log("Preparing to push to Github")
     getChanges(client).then(changes => {
       if (!changes.length) return Promise.resolve('No new changes')
       let emoji = emojis[random(0, emojis.length - 1)]
-      let msg = `${emoji} ${truncate(changes.join(', ').replace(/ClientExtracted/g, 'Client'), { length: 1000 })}`
-      let cmd = config.noPush ? `git commit -m "${msg}"` : `git commit -m "${msg}" && git push`
+      let msg = `${emoji} ${truncate(changes.join(', ').replace(/ClientExtracted(\/src)?/g, 'Client'), { length: 1000 })}`
+      let cmd = `git commit -m ${msg} ${config.noPush ? '' : '&& git push'}`
       exec(cmd, (err, stdout) => {
         console.log(err, stdout)
         if (err) return reject(err)
-        resolve(`Sucessfully committed changed ${config.noPush ? 'but did not push' : 'and pushed'} to Github`)
+        return resolve(`Sucessfully committed changed ${config.noPush ? 'but did not push' : 'and pushed'} to Github`)
       })
     }).catch(reject)
   })
 }
 
-function startTheMagic() {
-  getPageScripts().then(getScripts).then(writeToDisk).then(() => pushToGit()).then(msg => {
-    console.log(msg)
-  }).catch(err => console.error("Error while doing stuff", err))
+// Check if Slack has released a new version of its client and if it has, download it
+function checkClientVersion() {
+  request(clientReleasesURL, (err, resp, body) => {
+    if (err || !body) return console.error("Error fetching client releases", err)
+    let releases = compact(body.split('\n').map(rel => {
+      if (rel.includes('delta')) return null
+      let [ hash, release, size ] = rel.split(' ')
+      return { hash, release, size, version: release.split('-')[1] }
+    }))
+    let latestRelease = last(releases)
+    fs.readFile('./ClientExtracted/VERSION', 'utf8', (err, data) => {
+      let currentVersion = err ? null : data
+      if (currentVersion != latestRelease.version) clientUpdater.update(latestRelease).then(pushToGit)
+      else console.log("Slack Client hasn't updated")
+    })
+  })
 }
 
-function checkClientVersion() {
-  request(ClientReleasesURL, (err, resp, body) => {
-    if (!err && body) {
-      let releases = compact(body.split('\n').map(rel => {
-        if (rel.includes('delta')) return null
-        let [ hash, release, size ] = rel.split(' ')
-        return { hash, release, size, version: release.split('-')[1] }
-      }))
-      let latestRelease = last(releases)
-      fs.readFile('./ClientExtracted/VERSION', 'utf8', (err, data) => {
-        let currentVersion = err ? null : data
-        if (currentVersion != latestRelease.version) {
-          clientUpdater.update(latestRelease).then(pushToGit)
+function pullLatestChanges() {
+  return new Promise((resolve, reject) => {
+    console.log("Pulling latest changes, if any")
+    exec('git rev-parse HEAD', (err, lastCommit) => {
+      if (err || !lastCommit) return reject(`Error fetching last commit, ${err}`)
+      exec('git pull', (err, stdout) => {
+        if (!err && stdout && (stdout.startsWith('Updating') || stdout.includes('Already up-to-date'))) return resolve()
+        else {
+          console.error("Error while updating, reverting")
+          exec(`git reset ${lastCommit} --hard`, () => process.exit())
         }
-        else console.log("Slack Client hasn't updated")
       })
-    } else console.error("Error fetching client releases", err)
+    })
   })
+}
+
+function startTheMagic() {
+  pullLatestChanges()
+    .then(getPageScripts)
+    .then(getIndividualScripts)
+    .then(writeToDisk)
+    .then(() => pushToGit())
+    .then(console.log)
+    .catch(err => console.error("Error while doing stuff", err))
 }
 
 // Create directories if they don't exist
@@ -127,6 +149,7 @@ function checkDirectories() {
   fs.stat('./ClientExtracted', err => err ? fs.mkdir('./ClientExtracted') : void 0)
 }
 
+// Check scripts and client for updates every x mins
 setInterval(() => {
   console.log("Checking for updates")
   startTheMagic()
@@ -135,6 +158,7 @@ setInterval(() => {
 
 console.log("Starting up")
 
+// Init
 checkDirectories()
 startTheMagic()
 checkClientVersion()
