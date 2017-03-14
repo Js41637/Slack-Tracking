d8159d81e66ac069766e6105605b2c8ded3786a5
Slack-Tracker <pigeonbot@outlook.com>
2017-03-13 20:43:35 -0700

    :unlock: M Scripts/rollup-client.js

--- Scripts/rollup-client.js
+++ Scripts/rollup-client.js
@@ -21650,7 +21650,7 @@
           _loading_scripts = true;
           TS.utility.getCachedScript(cdn_url + "/cb0fd/js/libs/codemirror.js").done(function() {
             TS.utility.getCachedScript(cdn_url + "/7adb/js/libs/codemirror/addon/simple.js").done(function() {
-              TS.utility.getCachedScript(cdn_url + "/0375/js/codemirror_load.js").done(function() {
+              TS.utility.getCachedScript(cdn_url + "/d8d34/js/codemirror_load.js").done(function() {
                 TS.ui.snippet_dialog.start(text, title, filetype);
               });
             });
